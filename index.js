6ae4930fd591202b4616bfe965f6aad819d4afb6
Robert Riley <rob_a_riley@yahoo.com>
2018-10-13 23:36:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector){
+ return document.querySelector(selector);
+}
+
+function nestedTarget(){
+ return document.getElementById('nested').querySelector('.target'); 
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('.ranked-list');
+  
+  for (let i=0; i < lis.length; i++){
+   lis[i].innerHTML = (parseInt(lis[i].innerHTML,10) + n ).toString();
+   }
+}
+
+function deepestChild(){
+  const parent = document.getElementById('grand-node').querySelectorAll('div');
+  var child;
+  for (let i = 0; i < parent.length; i++){
+    child = parent[i];
+  }
+  return (child.innerHTML);
+}
